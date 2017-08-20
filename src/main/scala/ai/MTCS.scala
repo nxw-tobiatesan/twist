8983c2a57a73d9729732bf2c8f13ee9395b0ed6d
Tobia Tesan <tobia.tesan@gmail.com>
2017-08-21 00:47:32 +0200

    Clean up MTCS

--- src/main/scala/ai/MTCS.scala
+++ src/main/scala/ai/MTCS.scala
@@ -22,57 +22,65 @@ import com.tobiatesan.twist.player.{Player, AI, DebugStats, MoveWithStats}
 import scala.util.Random
 import collection.immutable.Map
 
-class UCTStats[G <: Game[G]](val nodes: Int,
-                             val simulations: Int,
-                             val tree: UCTNode[G])
+/////////////////////////////////////////
+// Stats
+/////////////////////////////////////////
+
+class UCTStats[G <: Game[G]](val nodes: Int, val tree: UCTNode[G])
     extends DebugStats[UCTAgent[G]] {
-  def +(v: UCTStats[G]) =
-    new UCTStats(nodes + v.nodes, simulations + v.simulations, tree)
   override def toString() =
-    "UCTNodes: " + nodes + "; Simulations: " + simulations + " Tree:===============\n" + tree + "\n====================\n"
+    "UCTNodes: " + nodes + "; Tree:===============\n" + tree + "\n====================\n"
   def getNodes = nodes
 }
+
+/////////////////////////////////////////
 
 case class UCTNode[G <: Game[G]](val position: LivePosition[G],
                                  val num: Int,
                                  val den: Int,
                                  val maxNode: Boolean,
-                                 val desc: Map[Move[G], UCTNode[G]]) {
+                                 val children: Map[Move[G], UCTNode[G]]) {
   override def toString: String =
-    (if (maxNode) "[+] (" else "[-] (") + num + "/" + den + ")\n\t" + desc
+    (if (maxNode) "[+] (" else "[-] (") + num + "/" + den + ")\n\t" + children
       .mkString("\n")
       .replaceAll("\n", "\n\t")
 }
 
-class UCTAgent[G <: Game[G]](
-    val budget: Int,
-    val c: Double, // The larger it is, the more the algorithm favors exploration over exploitation.
-    val maximize: Boolean = false,
-    val r: Random)
+/**
+  * Implements an (inefficient) UCT agent based on Gelly 2011
+  *
+  * @param budget the number of total nodes (including default policy) to explore before stopping
+  * @param c the exploration constant; the larger it is, the more the algorithm favors exploration
+  *  over exploitation.
+  */
+class UCTAgent[G <: Game[G]](val budget: Int,
+                             val c: Double,
+                             val maximize: Boolean = false,
+                             val r: Random)
     extends AI[G] {
   type Utility = Int
-  type Nodes = Int
+  type NodeCount = Int
+
   def SimDefault[G <: Game[G]](
-      p: Either[LivePosition[G], TerminalPosition[G]]): (Utility, Nodes) =
-    // Repeatedly pick a random move and return utility of reached state
+      p: Either[LivePosition[G], TerminalPosition[G]]): (Utility, NodeCount) =
     p match {
       case Right(t: TerminalPosition[G]) => (t.utility, 0)
       case Left(l: LivePosition[G]) => {
         val map = l.successor()
-        val picked = r.shuffle(map.keys.toList).head
-        val (util, nodes) = SimDefault((map.get(picked).get))
+        val randomMove = r.shuffle(map.keys.toList).head
+        val (util, nodes) = SimDefault((map.get(randomMove).get))
         (util, nodes + 1)
       }
     }
 
-  def TreeSelectMove[G <: Game[G]](p: LivePosition[G],
-                                   t: UCTNode[G],
-                                   c: Double): Move[G] = {
+  def SelectMove[G <: Game[G]](p: LivePosition[G],
+                               t: UCTNode[G],
+                               c: Double): Move[G] = {
     def N_s: Double = t.den.toDouble
     def N_s_a(a: Move[G]): Double =
-      t.desc.get(a).map(_.den).getOrElse(0).toDouble
+      t.children.get(a).map(_.den).getOrElse(0).toDouble
     def Q_s_a(a: Move[G]): Double =
-      t.desc.get(a).map((a) => a.num / a.den).getOrElse(0).toDouble
+      t.children.get(a).map((a) => a.num / a.den).getOrElse(0).toDouble
     def argmax[A](a: Seq[A], f: A => Double) = a.sortBy[Double](f).last
     def argmin[A](a: Seq[A], f: A => Double) = a.sortBy[Double](f).head
     if (t.maxNode)
@@ -83,37 +91,43 @@ class UCTAgent[G <: Game[G]](
              (a: Move[G]) => Q_s_a(a) - c * Math.sqrt(Math.log(N_s) / N_s_a(a)))
   }
 
+  /**
+    * @return Updated tree, spent budget and value of last simulation to be propagated
+    */
   def Simulate(p: LivePosition[G],
-               t: UCTNode[G]): (UCTNode[G], Nodes, Utility) = {
-    val move = TreeSelectMove(p, t, c) //, debug)
+               node: UCTNode[G]): (UCTNode[G], NodeCount, Utility) = {
+    val move = SelectMove(p, node, c)
     p.successor.get(move).get match {
-      case (Right(term)) =>
-        ((sim: (Utility, Nodes)) =>
-          (UCTNode(p, t.num + sim._1, t.den + 1, t.maxNode, t.desc),
+      case (Right(t)) =>
+        ((sim: (Utility, NodeCount)) =>
+          (UCTNode(p,
+                   node.num + sim._1,
+                   node.den + 1,
+                   node.maxNode,
+                   node.children),
            sim._2 + 1,
-           sim._1))(SimDefault(Right(term)))
+           sim._1))(SimDefault(Right(t)))
       case (Left(l)) =>
-        if (t.desc contains move) {
-          // Not a terminal state i.e. already in tree and not terminal
-          ((rec: (UCTNode[G], Nodes, Utility)) => {
-            require(rec._1.position == l)
+        if (node.children contains move) {
+          // Already in tree and not terminal, continue with tree policy
+          ((rec: (UCTNode[G], NodeCount, Utility)) => {
             (UCTNode(p,
-                     t.num + rec._3, // Backup
-                     t.den + 1,
-                     t.maxNode,
-                     t.desc updated (move, rec._1)),
+                     node.num + rec._3, // Backup
+                     node.den + 1,
+                     node.maxNode,
+                     node.children updated (move, rec._1)),
              rec._2 + 1,
              rec._3)
-          })(Simulate(l, t.desc.get(move).get))
+          })(Simulate(l, node.children.get(move).get))
         } else {
-          // Not in tree, not terminal
-          ((sim: (Utility, Nodes)) => {
+          // Not in tree, not terminal: expland
+          ((sim: (Utility, NodeCount)) => {
             (UCTNode[G](p,
-                        t.num + sim._1,
-                        t.den + 1,
-                        t.maxNode,
-                        (t.desc updated (move,
-                        UCTNode(l, sim._1, 1, !(t.maxNode), Map.empty)))),
+                        node.num + sim._1,
+                        node.den + 1,
+                        node.maxNode,
+                        (node.children updated (move,
+                        UCTNode(l, sim._1, 1, !(node.maxNode), Map.empty)))),
              sim._2 + 1,
              sim._1)
           })(SimDefault(Left(l)))
@@ -122,23 +136,21 @@ class UCTAgent[G <: Game[G]](
   }
 
   def UCTSearch(p: LivePosition[G],
-                budget: Nodes): (Move[G], Nodes, UCTNode[G], Int) = {
-    var t = new UCTNode[G](p, 0, 0, maximize, Map.empty)
-    var remainingbudget = budget
-    var simulations = 0
-    while (remainingbudget > 0) {
-      val (newt, total, _) = Simulate(p, t)
-      t = newt
-      remainingbudget = remainingbudget - total
-      simulations = simulations + 1
+                budget: NodeCount): (Move[G], NodeCount, UCTNode[G]) = {
+    var tree = new UCTNode[G](p, 0, 0, maximize, Map.empty)
+    var budget_* = budget
+    while (budget_* > 0) {
+      val (newtree, spent, _) = Simulate(p, tree)
+      tree = newtree
+      budget_* = budget_* - spent
     }
-    (TreeSelectMove(p, t, 0), budget - remainingbudget, t, simulations)
+    (SelectMove(p, tree, 0), budget - budget_*, tree)
   }
 
   def debug(
       p: LivePosition[G]): MoveWithStats[G, Move[G], DebugStats[UCTAgent[G]]] =
-    ((x: (Move[G], Int, UCTNode[G], Int)) =>
+    ((x: (Move[G], Int, UCTNode[G])) =>
       MoveWithStats[G, Move[G], DebugStats[UCTAgent[G]]](
         x._1,
-        new UCTStats(x._2, x._4, x._3)))(UCTSearch(p, budget))
+        new UCTStats(x._2, x._3)))(UCTSearch(p, budget))
 }
