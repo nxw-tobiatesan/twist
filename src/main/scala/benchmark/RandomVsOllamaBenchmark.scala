3169c78ece82e3cb61a6aacdab41176705e1dd6c
Tobia Tesan <tobia.tesan@gmail.com>
2025-09-10 16:13:01 +0200

    Add scratch RandomVsOllama benchmark

---
+++ src/main/scala/benchmark/RandomVsOllamaBenchmark.scala
@@ -0,0 +1,47 @@
+package com.tobiatesan.twist
+import com.tobiatesan.twist.ai.minimax.{
+  BasicAlphaBeta,
+  KillerAlphaBeta,
+  SimpleKillerAlphaBeta,
+  QuiescenceAlphaBeta
+}
+import com.tobiatesan.twist.ai.mtcs.{UCTAgent}
+import com.tobiatesan.twist.ai.{RandomAgent, OllamaAgent}
+import com.tobiatesan.twist.draughts.{Draughts, LiveDraughtsPosition}
+import com.tobiatesan.twist.matches.{Match, MatchLog, PlyEntry}
+import com.tobiatesan.twist.game.{
+  Move,
+  Game,
+  LivePosition,
+  TerminalPosition,
+  Min,
+  Max
+}
+import com.tobiatesan.twist.player.{AI}
+import java.io.File
+import com.github.tototoshi.csv.CSVWriter
+
+object RandomVsOllamaBenchmark extends App {
+  import com.tobiatesan.twist.benchmark._
+  val rounds = 40
+
+  new CSVPlusScreenPrinter[BasicMetric](
+    new File("benchmark/random_vs_ollama.csv")
+  ).prin(
+    Stream(
+      new BasicExtractor(
+        new Tournament(
+          Rounds.Large,
+          Stream.from(0).map(new scala.util.Random(_)),
+          (r: scala.util.Random) =>
+          new Match(
+            new RandomAgent(r),
+            new OllamaAgent[Draughts](model = "gemma3"),
+            false,
+            basicDraughts.startingPosition
+          )
+        )
+      ).extract()
+    )
+  )
+}
